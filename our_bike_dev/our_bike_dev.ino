4e5ae86c5d2d7f56920ca91754ae3fa581e28384
Jan Łuczka <jasiu.luczka@gmail.com>
2026-05-20 21:41:11 +0200

    poprawka

--- our_bike_dev/our_bike_dev.ino
+++ our_bike_dev/our_bike_dev.ino
@@ -187,8 +187,8 @@ void PID_controller() {
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  acc = -accel.y() * DEG_TO_RAD;                    
-  velocity = gyro.y() * DEG_TO_RAD;       
+  acc = -accel.y();                    
+  velocity = gyro.y();       
   
   // --- NOWOŚĆ: Aplikujemy mechaniczny offset przed przejściem na radiany ---
   float corrected_angle_deg = -euler.y() - angle_offset;
